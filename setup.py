089a4d0d56ebdbd2d0fe59f1e5ec663e5c527996
john <johnr@aluminumtrailer.com>
2019-09-06 13:36:08 -0500

    Put 'python' in front of all manage.py commands

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='hey-helper',
-    version='0.0.4',
+    version='0.0.5',
     author='John Rork',
     author_email='jnrork@gmail.com',
     description='A python wrapper for bash-like scripts',
